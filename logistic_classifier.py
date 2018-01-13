7648d7500344dbbf2a29a066a316341119d3c33e
Tony <yhtcoding@gmail.com>
2018-01-13 11:12:00 -0800

    write 'make_train_data' and 'make_test_data' and make notation informative

--- logistic_classifier.py
+++ logistic_classifier.py
@@ -2,33 +2,56 @@ import numpy as np
 import load_train_data
 import load_test_data
 from logistic_regression import *
+import matplotlib.pyplot as plt
+
+def plot_weights(Weights):
+    plt.imshow(Weights[0:-1].reshape(28,28))
+    plt.show()
+
+def plot_accuracy(accuracy):
+    x = np.arange(len(accuracy))
+    y = np.array(accuracy)
+
+    plt.plot(x,y)
+    plt.show()
+
+
+def make_train_data(numberA,numberB):
+    train_image_A = load_train_data.fetch_image(numberA)
+    train_image_B = load_train_data.fetch_image(numberB)
+    train_input_A = np.concatenate((train_image_A, np.ones((1, train_image_A.shape[0])).T), axis=1).T
+    train_input_B = np.concatenate((train_image_B, np.ones((1, train_image_B.shape[0])).T), axis=1).T
+    train_target = np.concatenate((np.ones((1, train_input_A.shape[1])), np.zeros((1, train_input_B.shape[1]))), axis=1).T
+    train_input = np.concatenate((train_input_A, train_input_B), axis=1)
+
+
+    #make hold out set
 
 
 
+    return train_input,train_target
+
+def make_test_data(numberA,numberB):
+    test_image_A = load_test_data.fetch_image(numberA)
+    test_image_B = load_test_data.fetch_image(numberB)
+    test_input_A = np.concatenate((test_image_A, np.ones((1, test_image_A.shape[0])).T), axis=1).T
+    test_input_B = np.concatenate((test_image_B, np.ones((1, test_image_B.shape[0])).T), axis=1).T
+    test_target = np.concatenate((np.ones((1, test_input_A.shape[1])), np.zeros((1, test_input_B.shape[1]))), axis=1).T
+    test_input = np.concatenate((test_input_A, test_input_B), axis=1)
+
+    return test_input,test_target
 
 
 if __name__ == "__main__":
-    images_train_2 = load_train_data.fetch_image(2)
-    images_train_3 = load_train_data.fetch_image(3)
-    train_input_2 = np.concatenate((images_train_2, np.ones((1, images_train_2.shape[0])).T), axis=1).T
-    train_input_3 = np.concatenate((images_train_3, np.ones((1, images_train_3.shape[0])).T), axis=1).T
-    train_target = np.concatenate((np.ones((1, train_input_2.shape[1])), np.zeros((1, train_input_3.shape[1]))), axis=1).T
-    train_input = np.concatenate((train_input_2, train_input_3), axis=1)
 
-    W,steps = gradient(train_input, train_target)
+    train_input_2_vs_3,train_target_2_vs_3 = make_train_data(2,3)
 
+    Weights_2_vs_3,accuracy_2_vs_3 = gradient(train_input_2_vs_3, train_target_2_vs_3)
 
+    test_input_2_vs_3,test_target_2_vs_3 = make_test_data(2,3)
 
-    images_test_2 = load_test_data.fetch_image(2)
-    images_test_3 = load_test_data.fetch_image(3)
-    test_input_2 = np.concatenate((images_test_2, np.ones((1, images_test_2.shape[0])).T), axis=1).T
-    test_input_3 = np.concatenate((images_test_3, np.ones((1, images_test_3.shape[0])).T), axis=1).T
-    test_target = np.concatenate((np.ones((1, test_input_2.shape[1])), np.zeros((1, test_input_3.shape[1]))), axis=1).T
-    test_input = np.concatenate((test_input_2, test_input_3), axis=1)
-
-    accuracy = 1 - (sum(sigmoid(W, test_input_2) < 0.5) + sum(sigmoid(W, test_input_3) > 0.5)) / (test_input_2.shape[1] + test_input_3.shape[1])
-    print(accuracy)
-    print(steps)
+    plot_weights(Weights_2_vs_3)
+    plot_accuracy(accuracy_2_vs_3)
 
 
 
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,25 +1,27 @@
 import numpy as np
 
-def sigmoid(W,X):
 
-    Y = 1 / (1 + np.exp( -np.dot(W.T,X)))
+def check(W,X,Target):
+    return np.sum(np.abs(np.round(sigmoid(W,X)) - Target))/Target.shape[0]
+
+def sigmoid(W,X):
+    z = np.dot(W.T,X)
+    Y = 1.0 / (1.0 + np.exp(-z))
 
     return Y.T
 
-def gradient(X,T):
+def gradient(X,Target):
 
-    W = np.random.random(X.shape[0]).reshape(X.shape[0],1)
-    alpha = 0.001
-
-    # while np.linalg.norm(np.dot((T - sigmoid(W,X)).T,X.T)) > 0.0001:
-    #     W = W + alpha * np.dot((T - sigmoid(W,X)).T,X.T)
-    #     print(W)
-    steps = 0
-    while np.linalg.norm(np.dot(X,T-sigmoid(W,X))) > 0.1:
-        W = W + alpha * np.dot(X,T-sigmoid(W,X))
-        steps = steps + 1
-        print("%s-----%s"%(steps,np.linalg.norm(np.dot(X,T-sigmoid(W,X)))))
-    return W,steps
+    W = np.zeros((X.shape[0],1))
+    accuracy = []
+    eta_0 = 0.0001
+    T = 5
+    for epoch in range(1000):
+        eta = eta_0 / (1 + epoch / T)
+        W = W + eta * np.dot(X, Target -sigmoid(W, X))
+        # print(1-check(W,X,Target))
+        accuracy.append(1-check(W,X,Target))
+    return W,accuracy
 
 
 
@@ -27,4 +29,6 @@ if __name__ == "__main__":
     X = np.array([0.50,0.75,1.00,1.25,1.50,1.75,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,4.00,4.25,4.50,4.75,5.00,5.50])
     X = np.vstack((X,np.ones(len(X))))
     Y = np.array([0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,1,1,1,1,1]).reshape(X.shape[1],1)
-    print(gradient(X,Y))
+    W = gradient(X,Y)
+    print(check(W,X,Y))
+    print(np.round(sigmoid(W,X)))
