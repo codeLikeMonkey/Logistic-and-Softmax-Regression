fix loss function error and use average loss

logistic_classifier.py
@@ -38,9 +38,9 @@ def loss(Target,Y):
     E = 0
     # for n in range(len(Target)):
         # E = E + np.power(Target[n],Y[n]) * np.power(1-Y[n],Target[n])
-    E = -np.dot(np.log10(Y.T+1e-5),Target) + np.dot(np.log10(1 - Y.T+1e-5),1 - Target)
+    E = np.dot(np.log10(Y.T+1e-10),Target) + np.dot(np.log10(1 - Y.T+1e-10),1 - Target)
     # print()
-    return float(E)
+    return -float(E)/len(Target)
 
 
 # def loss_with_regularization(Target,Y,Weights):
@@ -65,24 +65,32 @@ def loss(Target,Y):
 #     return W,accuracy
 
 
-def regularized_gradient_descent(Lamada,train_set,train_target):
+def regularized_gradient_descent(type_of_regularization,Lamada,train_set,train_target):
     train_set_weights = np.zeros((train_set.shape[0],1))
     train_set_weights[-1] = 1
     train_set_accuracy = []
     train_set_loss = []
+    length_of_weights = []
     eta_0 = 0.001
-    T = 10
+    T = 100
     # Lamada = 0.01
 
-    for epoch in range(450):
+    for epoch in range(100):
         train_set_accuracy.append(1 - check(train_set_weights, train_set, train_target))
         #
         train_set_loss.append(loss(train_target, sigmoid(train_set_weights, train_set)))
 
         eta = eta_0 / (1 + epoch / T)
-        train_set_weights = train_set_weights + eta * (np.dot(train_set,train_target - sigmoid(train_set_weights, train_set)) + Lamada *2 * train_set_weights)
+        if type_of_regularization == "l2":
+            train_set_weights = train_set_weights + eta * (np.dot(train_set,train_target - sigmoid(train_set_weights, train_set)) - Lamada * 2 * train_set_weights)
+            # length_of_weights.append(np.linalg.norm(train_set_weights))
+        else:
+            train_set_weights = train_set_weights + eta * (np.dot(train_set,train_target - sigmoid(train_set_weights, train_set)) - Lamada * np.sign(train_set_weights))
 
-    return train_set_weights, train_set_accuracy
+        length_of_weights.append(np.linalg.norm(train_set_weights))
+
+
+    return length_of_weights,train_set_weights, train_set_accuracy
 
 
 
@@ -109,9 +117,9 @@ def gradient_descent(train_set,train_target,hold_out_set,hold_out_target,test_se
     hold_out_loss = []
     test_set_loss = []
     eta_0 = 0.001
-    T = 10
+    T = 100
     
-    for epoch in range(450):
+    for epoch in range(100):
 
         train_set_accuracy.append(1 - check(train_set_weights,train_set,train_target))
         hold_out_accuracy.append(1 - check(train_set_weights,hold_out_set,hold_out_target))
@@ -160,8 +168,10 @@ def plot_loss(loss,color):
     # plt.plot(x, y, marker='.', linestyle='-',color = color)
     plt.plot(x, y)
 
-
-
+def plot_length_of_weights(length_of_weights):
+    y = np.array(length_of_weights)
+    x= np.arange(len(length_of_weights))
+    plt.plot(x,y)
 
 def make_train_data(numberA,numberB):
     '''
@@ -229,7 +239,7 @@ if __name__ == "__main__":
     train_input_2vs3,train_target_2vs3,hold_out_input_2vs3,hold_out_target_2vs3 = make_train_data(2,3)
     test_input_2vs3, test_target_2vs3 = make_test_data(2, 3)
 
-    # train_Weights_2vs3,train_accuracy_2vs3,hold_out_accuracy_2vs3,test_set_accuracy_2vs3,train_set_loss_2vs3,hold_out_loss_2vs3,test_set_loss_2vs3 = gradient_descent(train_input_2vs3, train_target_2vs3,hold_out_input_2vs3,hold_out_target_2vs3,test_input_2vs3,test_target_2vs3)
+    train_Weights_2vs3,train_accuracy_2vs3,hold_out_accuracy_2vs3,test_set_accuracy_2vs3,train_set_loss_2vs3,hold_out_loss_2vs3,test_set_loss_2vs3 = gradient_descent(train_input_2vs3, train_target_2vs3,hold_out_input_2vs3,hold_out_target_2vs3,test_input_2vs3,test_target_2vs3)
     # #train_Weights_2vs3,train_accuracy_2vs3 = gradient(train_input_2vs3,train_target_2vs3)
     #
     # plt.figure(1)
@@ -249,26 +259,28 @@ if __name__ == "__main__":
     # plot_accuracy(test_set_accuracy_2vs3)
     # plt.show()
     #
-    # plt.figure(3)
-    # ax = plt.gca()
-    # # ax.grid()
-    # ax.set_title("Loss (E) over Training")
-    # ax.set_xlabel("Training Epochs")
-    # plot_loss(train_set_loss_2vs3,'r')
-    # plot_loss(hold_out_loss_2vs3,'b')
-    # plot_loss(test_set_loss_2vs3,'g')
+    plt.figure(3)
+    ax = plt.gca()
+    # ax.grid()
+    ax.set_title("Loss (E) over Training")
+    ax.set_xlabel("Training Epochs")
+    plot_loss(train_set_loss_2vs3,'r')
+    plot_loss(hold_out_loss_2vs3,'b')
+    plot_loss(test_set_loss_2vs3,'g')
     # ax.legend(['Training Set','Hold Out Set','Test Set'])
-    # plt.show()
+    plt.show()
 
 
     #regulized logistic regression
-    LAMADA = [2,1,0.1,0.001,0.00001]
-    for Lamada in LAMADA:
-        reg_train_Weights_2vs3,reg_train_accuracy_2vs3=regularized_gradient_descent(Lamada,train_input_2vs3,train_target_2vs3)
-        plot_accuracy(reg_train_accuracy_2vs3)
-    ax = plt.gca()
-    ax.legend(["lamada = 2","lamada = 1","lamada = 0.1","lamada = 0.001","lamada = 0.00001"])
-    plt.show()
+    # LAMADA = [2,1,0.1,0.001,0.00001]
+    # for Lamada in LAMADA:
+    #     length_of_weights,reg_train_Weights_2vs3,reg_train_accuracy_2vs3=regularized_gradient_descent('l2',Lamada,train_input_2vs3,train_target_2vs3)
+    #     #plot _accuracy of training set over epochs with different lamada
+    #     # plot_accuracy(reg_train_accuracy_2vs3)
+    #     plot_length_of_weights(length_of_weights)
+    # ax = plt.gca()
+    # ax.legend(["lamada = 2","lamada = 1","lamada = 0.1","lamada = 0.001","lamada = 0.00001"])
+    # plt.show()
 
 
 