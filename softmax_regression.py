0c5bed27552c156f6dc6976d09efdcb404424620
Tony <yhtcoding@gmail.com>
2018-01-15 12:54:37 -0800

    finish minibatch, but need to add test set accuracy and many plots....

--- softmax_regression.py
+++ softmax_regression.py
@@ -27,9 +27,13 @@ def gradient_descent(Lamada, train_set, train_target):
     train_set_accuracy = []
     train_weights_mat = np.zeros((785,10))
     train_weights_mat[-1,:] = 1
+    # hold_out_set = train_set_all[:,0:train_set_all.shape[1]//10]
+    # hold_out_target = train_target_all[0:train_set_all.shape[1]//10,:]
+    # train_set = train_set_all[:,train_set_all.shape[1]//10:]
+    # train_target = train_target_all[train_set_all.shape[1]//10:,:]
 
     max_epoch = 4
-    number_of_mini_batches = 10
+    number_of_mini_batches = 100
 
     # batches_sizes = np.array([train_set.shape[1]//number_of_mini_batches] * number_of_mini_batches)
     random_index = np.arange(train_set.shape[1]// number_of_mini_batches * number_of_mini_batches)
@@ -38,16 +42,44 @@ def gradient_descent(Lamada, train_set, train_target):
     random_mini_batches_set  = [train_set[:,x ]for x in np.split(random_index,number_of_mini_batches)]
     random_mini_batches_target = [train_target[x,:]for x in np.split(random_index,number_of_mini_batches)]
 
+    #make hold_out set
+    hold_out_set = random_mini_batches_set[-1]
+    hold_out_target = random_mini_batches_target[-1]
 
-    eta_0 = 0.01
+    #make training set
+    random_mini_batches_target = random_mini_batches_target[0:-1]
+    random_mini_batches_set = random_mini_batches_set[0:-1]
+    hold_out_accuracy = []
+
+
+    eta_0 = 0.001
     T = 50
+    update_times = 0
 
-    for update_times in range(500):
-        eta = eta_0 / (1 + update_times / T)
-        train_set_accuracy.append(check(train_weights_mat,train_set,train_target))
-        train_weights_mat = train_weights_mat + eta * (np.dot(train_set, train_target - sigmoid(train_weights_mat, train_set)) - Lamada * 2 * train_weights_mat)
+    for epoch in range(max_epoch):
+        for i in range(len(random_mini_batches_set)):
+            # print("mini_batch:%s"%i)
+            #ith mini_batch_set and mini_batch_target
+            update_times += 1
+            eta = eta_0 / (1 + update_times / T)
+            train_set_accuracy.append(check(train_weights_mat,train_set,train_target))
+            hold_out_accuracy.append(check(train_weights_mat,hold_out_set,hold_out_target))
+            # print("update times : %s accuracy %s" % (update_times,train_set_accuracy[-1]))
+            train_weights_mat = train_weights_mat + eta * (np.dot(random_mini_batches_set[i],random_mini_batches_target[i] - sigmoid(train_weights_mat,random_mini_batches_set[i])) - Lamada * 2 * train_weights_mat)
+        if len(train_set_accuracy)>4 and train_set_accuracy[-4]<train_set_accuracy[-3]<train_set_accuracy[-2]<train_set_accuracy[-1]:
+            break
+        # if len(hold_out_accuracy)>4 and hold_out_accuracy[-4]<hold_out_accuracy[-3]<hold_out_accuracy[-2]<hold_out_accuracy[-1]:
+        #     break
 
-    return train_set_accuracy
+
+
+
+    # for update_times in range(500):
+    #     eta = eta_0 / (1 + update_times / T)
+    #     train_set_accuracy.append(check(train_weights_mat,train_set,train_target))
+    #     train_weights_mat = train_weights_mat + eta * (np.dot(train_set, train_target - sigmoid(train_weights_mat, train_set)) - Lamada * 2 * train_weights_mat)
+
+    return train_set_accuracy,hold_out_accuracy
 
 
 def plot_accuracy(accuracy):
@@ -68,8 +100,8 @@ def make_train_data():
     mndata.gz = True
     images, labels = mndata.load_training()
 
-    images = images[0:30]
-    labels = labels[0:30]
+    images = images[0:20000]
+    labels = labels[0:20000]
 
     # intialize target maxtrix
     train_target = np.zeros((len(labels),10))
@@ -89,8 +121,11 @@ if __name__ == "__main__":
     train_set, train_target = make_train_data()
     # train_weights : 785 * 10
     #
-    train_accuracy = gradient_descent(0.1,train_set,train_target)
+    train_accuracy,hold_out_accuracy = gradient_descent(0.1,train_set,train_target)
     plot_accuracy(train_accuracy)
+    plot_accuracy(hold_out_accuracy)
+    plt.gca().legend(["train","hold out"])
+
     plt.show()
 
 
