a51c8c0486332ec58310b0eef566c1f14937ae0a
Tony <yhtcoding@gmail.com>
2018-01-16 21:11:28 -0800

    update

--- logistic_classifier.py
+++ logistic_classifier.py
@@ -116,51 +116,74 @@ def gradient_descent(train_set,train_target,hold_out_set,hold_out_target,test_se
 
 
 
-def mini_batch_gradient_descent(train_set,train_target):
-    train_set_weights = np.zeros((train_set.shape[0], 1))
-    train_set_weights[-1] = 1
-    train_set_accuracy = []
-    hold_out_accuracy = []
-    test_set_accuracy = []
-    train_set_loss = []
-    hold_out_loss = []
-    test_set_loss = []
-    eta_0 = 0.001
-    T = 100
-    how_many_mini_batches = 10
+def mini_gradient_descent(Lamada, train_set, train_target,test_set,test_target):
 
-    batches_sizes = [train_set.shape[1] // how_many_mini_batches] * how_many_mini_batches
-    random_index = np.arange((train_set.shape[1]//how_many_mini_batches) * how_many_mini_batches)
-    np.random.shuffle(random_index)
-    random_mini_batches_set = np.array([train_set[:,x] for x in np.split(random_index,how_many_mini_batches)])
-    random_mini_batches_target = np.array([train_target[x].squeeze() for x in np.split(random_index, how_many_mini_batches)])
+    train_set_accuracy = []
+    test_set_accuracy = []
+    train_weights_mat = np.zeros((785,1))
+    train_weights_mat[-1,:] = 1
+
+    train_loss = []
+    hold_out_loss = []
+    test_loss = []
+    # hold_out_set = train_set_all[:,0:train_set_all.shape[1]//10]
+    # hold_out_target = train_target_all[0:train_set_all.shape[1]//10,:]
+    # train_set = train_set_all[:,train_set_all.shape[1]//10:]
+    # train_target = train_target_all[train_set_all.shape[1]//10:,:]
 
     max_epoch = 4
+    number_of_mini_batches = 100
 
+    # batches_sizes = np.array([train_set.shape[1]//number_of_mini_batches] * number_of_mini_batches)
+    random_index = np.arange(train_set.shape[1]// number_of_mini_batches * number_of_mini_batches)
+    np.random.shuffle(random_index)
+
+    random_mini_batches_set  = [train_set[:,x ]for x in np.split(random_index,number_of_mini_batches)]
+    random_mini_batches_target = [train_target[x,:]for x in np.split(random_index,number_of_mini_batches)]
+
+    #make hold_out set
+    hold_out_set = random_mini_batches_set[-1]
+    hold_out_target = random_mini_batches_target[-1]
+
+    #make training set
+    random_mini_batches_target = random_mini_batches_target[0:-1]
+    random_mini_batches_set = random_mini_batches_set[0:-1]
+    hold_out_accuracy = []
+
+
+
+    eta_0 = 0.0001
+    T = 50
     update_times = 0
 
     for epoch in range(max_epoch):
-        for i in range(how_many_mini_batches):
-            update_times +=1
-            eta = eta = eta_0 / (1 + update_times / T)
-            train_set_weights = train_set_weights + eta * np.dot(random_mini_batches_set[i],random_mini_batches_target - sigmoid(train_set_weights,random_mini_batches_set[i]))
-            train_set_accuracy.append(1 - check(train_set_weights, train_set, train_target))
-    return train_set_weights,train_set_accuracy
+        for i in range(len(random_mini_batches_set)):
+            # print("mini_batch:%s"%i)
+            #ith mini_batch_set and mini_batch_target
+            update_times += 1
+            eta = eta_0 / (1 + update_times / T)
+            train_set_accuracy.append(check(train_weights_mat,train_set,train_target))
+            hold_out_accuracy.append(check(train_weights_mat,hold_out_set,hold_out_target))
+            test_set_accuracy.append(check(train_weights_mat,test_set,test_target))
+            train_loss.append(loss(train_target,sigmoid(train_weights_mat,train_set)))
+            hold_out_loss.append(loss(hold_out_target,sigmoid(train_weights_mat,hold_out_set)))
+            test_loss.append(loss(test_target,sigmoid(train_weights_mat,test_set)))
+            # print("update times : %s accuracy %s" % (update_times,train_set_accuracy[-1]))
+            train_weights_mat = train_weights_mat + eta * (np.dot(random_mini_batches_set[i],random_mini_batches_target[i] - sigmoid(train_weights_mat,random_mini_batches_set[i])) - Lamada * 2 * train_weights_mat)
+        # if len(train_set_accuracy)>4 and train_set_accuracy[-4]<train_set_accuracy[-3]<train_set_accuracy[-2]<train_set_accuracy[-1]:
+        #     break
+        if len(hold_out_accuracy)>4 and hold_out_accuracy[-4]<hold_out_accuracy[-3]<hold_out_accuracy[-2]<hold_out_accuracy[-1]:
+            break
 
 
-    # for epoch in range(100):
-    #     train_set_accuracy.append(1 - check(train_set_weights, train_set, train_target))
-    #     # hold_out_accuracy.append(1 - check(train_set_weights, hold_out_set, hold_out_target))
-    #     # test_set_accuracy.append(1 - check(train_set_weights, test_set, test_target))
-    #     #
-    #     train_set_loss.append(loss(train_target, sigmoid(train_set_weights, train_set)))
-    #     # hold_out_loss.append(loss(hold_out_target, sigmoid(train_set_weights, hold_out_set)))
-    #     # test_set_loss.append(loss(test_target, sigmoid(train_set_weights, test_set)))
-    #
-    #     eta = eta_0 / (1 + epoch / T)
-    #     train_set_weights = train_set_weights + eta * np.dot(train_set,train_target - sigmoid(train_set_weights, train_set))
 
-    return train_set_weights, train_set_accuracy#, hold_out_accuracy, test_set_accuracy, train_set_loss, hold_out_loss, test_set_loss
+
+    # for update_times in range(500):
+    #     eta = eta_0 / (1 + update_times / T)
+    #     train_set_accuracy.append(check(train_weights_mat,train_set,train_target))
+    #     train_weights_mat = train_weights_mat + eta * (np.dot(train_set, train_target - sigmoid(train_weights_mat, train_set)) - Lamada * 2 * train_weights_mat)
+
+    return train_weights_mat,train_set_accuracy,hold_out_accuracy,test_set_accuracy,train_loss,hold_out_loss,test_loss
 
         
     
@@ -307,11 +330,11 @@ if __name__ == "__main__":
     for Lamada in LAMADA:
         length_of_weights,reg_train_Weights_2vs3,reg_train_accuracy_2vs3,test_accuracy_2vs3=regularized_gradient_descent('l2',Lamada,train_input_2vs3,train_target_2vs3,test_input_2vs3,test_target_2vs3)
         ##plot _accuracy of training set over epochs with different lamada
-        # plot_accuracy(reg_train_accuracy_2vs3)
-        # ax = plt.gca()
-        # ax.set_title("Accuracy over Training with different lamada")
-        # ax.set_xlabel("Training Times")
-        # ax.set_ylabel("Accuracy")
+        plot_accuracy(reg_train_accuracy_2vs3)
+        ax = plt.gca()
+        ax.set_title("Accuracy over Training with different lamada")
+        ax.set_xlabel("Training Times")
+        ax.set_ylabel("Accuracy")
 
         ## plot length of weights
         # plot_length_of_weights(length_of_weights)
